c88cd48cb51406c130465c6d967a193fbe7bbf7a
Sshaonak P Galvankar <shaunakprasad@icloud.com>
2018-05-19 17:19:04 +0000

    Add files via upload

---
+++ pwmtest.py
@@ -0,0 +1,13 @@
+import os     #importing os library so as to communicate with the system
+import time   #importing time library to make Rpi wait because its too impatient 
+os.system ("sudo pigpiod") #Launching GPIO library
+time.sleep(1) # As i said it is too impatient and so if this delay is removed you will get an error
+import pigpio #importing GPIO library
+
+pi=pigpio.pi()
+
+
+pi.set_servo_pulsewidth(4,1000)
+time.sleep(1)
+
+
